60b4687569e18d5c81c15ce7d6d64db41c8f44a1
Kaio Pacifico Soares Formiga <109231738+Ryuase@users.noreply.github.com>
2022-08-15 06:22:56 -0300

    commit

---
+++ script.js
@@ -0,0 +1,101 @@
+let notaPortugues
+let notaMatematica
+let notaGeografia
+let notaHistoria
+let notaInformatica
+let notaIngles
+let notaFilosofia
+let notaSociologia
+let notaLiteratura
+let notaEducacaoFisica
+
+let presencaPortugues
+let presencaMatematica
+let presencaGeografia 
+let presencaHistoria
+let presencaInformatica
+let presencaIngles
+let presencaFilosofia
+let presencaSociologia
+let presencaLiteratura
+let presencaEducacaoFisica
+
+
+
+let nome = prompt("Entre com o nome do aluno")
+
+let materia = prompt("Digite a materia")
+
+
+
+if (materia=="nota")
+ 
+
+
+elseif(materia == "matematica", "Matematica","Portugues","portugues","geografia","Geografia","historia","Historia","informatica","Informatica","ingles","Ingles","filosofia","Filosofia","sociologia","Sociologia","literatura","Literatura") 
+
+    notaPortugues = parseFloat(prompt("Digita sua Nota"))
+    presencaPortugues = parseInt(prompt("Digite sua Presença"))
+
+    notaMatematica = parseFloat(prompt("Digita sua Nota"))
+    presencaMatematica = parseInt(prompt("Digite sua Presença"))
+
+    notaGeografia  = parseFloat(prompt("Digita sua Nota"))
+    presencaGeografia  = parseInt(prompt("Digite sua Presença"))
+
+    notaMaHistoria = parseFloat(prompt("Digita sua Nota"))
+    presencaHistoria = parseInt(prompt("Digite sua Presença"))
+
+    notaInformatica = parseFloat(prompt("Digita sua Nota"))
+    presencaInformatica = parseInt(prompt("Digite sua Presença"))
+
+    notaIngles = parseFloat(prompt("Digita sua Nota"))
+    presencaIngles = parseInt(prompt("Digite sua Presença"))
+
+    notaFilosofia = parseFloat(prompt("Digita sua Nota"))
+    presencaFilosofia = parseInt(prompt("Digite sua Presença"))
+
+    notaSociologia = parseFloat(prompt("Digita sua Nota"))
+    presencaSociologia = parseInt(prompt("Digite sua Presença"))
+
+    notaLiteratura = parseFloat(prompt("Digita sua Nota"))
+    presencaLiteratura = parseInt(prompt("Digite sua Presença"))
+
+    notaEducacaoFisica = parseFloat(prompt("Digita sua Nota"))
+    presencaEducacaoFisica = parseInt(prompt("Digite sua Presença"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(nome)
+console.log(notaMatematica, presencaMatematica);
+console.log(notaPortugues, presencaPortugues);
+console.log(notaGeografia, presencaGeografia);
+console.log(notaFilosofia,presencaFilosofia);
+console.log(notaIngles, presencaIngles);
+console.log(notaLiteratura, presencaLiteratura);
+console.log(notaInformatica, presencaInformatica)
+console.log(notaHistoria, presencaHistoria)
+console.log(notaEducacaoFisica, presencaEducacaoFisica)
+console.log(notaSociologia,presencaSociologia)
+
+
+
+
+
+
+
+
+
+
